d1d2d283caa459ccf4a1ad1eb8167451a2f098cd
Jeff <darth.jeff1@gmail.com>
2016-08-07 19:50:59 +0100

    structural - flyweight

--- 04-structural/06-flyweight/flyweight01.js
+++ 04-structural/06-flyweight/flyweight01.js
@@ -1,1 +1,25 @@
-//
+// cache
+
+var api = {
+    getAllTeams: function() {
+        console.log('I am an API call');
+        return [
+            'WBA', 'Aston Villa', 'Birmingham City'
+        ];
+    }
+};
+
+var footballFacade = {
+    getAllTeams: function() {
+        if(!this._teams) {
+            this._teams = api.getAllTeams();
+        }
+        return this._teams;
+    }
+};
+
+var teams1 = footballFacade.getAllTeams();
+console.log(teams1);
+
+var teams2 = footballFacade.getAllTeams();
+console.log(teams2);
