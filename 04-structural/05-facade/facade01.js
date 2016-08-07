90839923c4a1889355f8cba538f3b25e4807257f
Jeff <darth.jeff1@gmail.com>
2016-08-07 19:43:33 +0100

    structural - facade

--- 04-structural/05-facade/facade01.js
+++ 04-structural/05-facade/facade01.js
@@ -1,1 +1,32 @@
 //
+
+var databaseConnection = {
+    open: function() {
+        this._open = true;
+        console.log('Open Database Connection');
+    },
+    close: function() {
+        this._open = false;
+        console.log('Close Database Connection');
+    },
+    query: function(q) {
+        if(this._open || false) {
+            console.log('Query Database: ' + q);
+        } else {
+            console.log('ERROR: Database Connection is Closed');
+        }
+    }
+};
+
+var customerFacade = {
+    getCustomerBySurname: function(surname) {
+        var conn = Object.create(databaseConnection);
+        conn.open();
+        conn.query('SELECT * FROM tbCustomer WHERE surname = ' + surname);
+        conn.close();
+    }
+};
+
+
+var facade = Object.create(customerFacade);
+facade.getCustomerBySurname('Skywalker');
