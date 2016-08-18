b88634f9909a651b2cdedf0cd2039f6e4e0ffe94
Jeff Brannon <Jeff.Brannon@cm2000.co.uk>
2016-08-18 10:28:14 +0100

    structural - proxy

--- 03-creational/01-factory/factory01.js
+++ 03-creational/01-factory/factory01.js
@@ -1,6 +1,26 @@
 // object creation
 
 var car = {
+    get make() {
+        return this._make || '';
+    },
+    set make (ma) {
+        this._make = ma;
+    },
+    get model() {
+        return this._model || '';
+    },
+    set model (mo) {
+        this._model = mo;
+    }
+};
+
+var car = {
+    make: 'Mercedes',
+    model: 'A Class'
+};
+
+var car = {
     init: function() {
         this.vehicleType = 'car';
     },
@@ -12,3 +32,21 @@ var car = {
 var vehicle1 = Object.create(car);
 vehicle1.init();
 vehicle1.whoAmI();
+
+
+vehicle1.name = 'jeff';
+
+var vehicle2 = Object.create(car);
+vehicle2.name = 'susie';
+
+
+console.log(vehicle1.name);
+console.log(vehicle2.name);
+
+var carMercedes = Object.create(car);
+carMercedes.make = 'Mercedes';
+carMercedes.model = 'A Class';
+
+var carAudi = Object.create(car);
+carMercedes.make = 'Audi';
+carMercedes.model = 'A3';
